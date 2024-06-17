4d5dc5623f77609cd2f98be0d7c5b4975a80aea6
luisTapiaIgnacio <resuresua@gmail.com>
2024-06-17 12:33:38 -0500

    1er commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,183 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+import { Formulario, Label, 
+  ContenedorTerminos, ContenedorBotonCentrado, Boton,MensajeExito,MensajeError } from './elementos/Formularios';
+
+//importamos el compotente de la carpeta componente / Input
+import ComponenteInput from './componentes/Input';
+
+
+const App = () => {
+  const[usuario, cambiarUsuario]=useState({campo: '', valido:null});
+  const[nombre, cambiarNombre]=useState({campo: '', valido:null});
+  const[password, cambiarPassword]=useState({campo: '', valido:null});
+  const[password2, cambiarPassword2]=useState({campo: '', valido:null});
+  const[correo, cambiarCorreo]=useState({campo: '', valido:null});
+  const[telefono, cambiarTelefono]=useState({campo: '', valido:null});
+  const[terminos, cambiarTerminos]=useState(false);
+  const[formularioValido, cambiarFormularioValido] = useState(null);
+  const [mostrarMensajeExito, setMostrarMensajeExito] = useState(false);
+
+
+  const expresiones = {
+    usuario: /^[a-zA-Z0-9_-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+  }
+  
+  //validar contraseña
+  const validarPassword2=()=>{
+    if(password.campo.length > 0){
+      if(password.campo !== password2.campo){
+        cambiarPassword2((prevState)=>{
+          return {...prevState, valido: 'false'}
+        });
+      }else{
+        cambiarPassword2((prevState)=>{
+          return {...prevState, valido: 'true'}
+        })
+    }
+    }
+  }
+
+  const onchangeTerminos=(e)=>{
+    cambiarTerminos(e.target.checked);
+  }
+
+  const onSubmit = (e)=>{
+    e.preventDefault();
+
+    if(usuario.valido ==='true' &&
+      nombre.valido==='true' &&
+      password.valido==='true' &&
+      password2.valido==='true' &&
+      correo.valido==='true' &&
+      telefono.valido === 'true' &&
+      terminos
+      ){
+        cambiarFormularioValido(true);
+        setMostrarMensajeExito(true);
+        setTimeout(() => {
+          setMostrarMensajeExito(false);
+        }, 3000);
+        cambiarUsuario({campo: '', valido:null});
+        cambiarNombre({campo: '', valido:null});
+        cambiarPassword({campo: '', valido:null});
+        cambiarPassword2({campo: '', valido:null});
+        cambiarCorreo({campo: '', valido:null});
+        cambiarTelefono({campo: '', valido:null});
+        
+      }else{
+        cambiarFormularioValido(false);
+      }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Formulario action="" onSubmit={onSubmit}>
+      
+        <ComponenteInput
+        estado={usuario}
+        cambiarEstado={cambiarUsuario}
+        tipo="text"
+         label="Usuario"
+         placeholder="Jhon conor"
+         name="usuario"
+         leyendaError="el suario tiene que ser de 4 a 16 digitos, tiene que contener numeros, letras y guion bajo"
+         expresionRegular={expresiones.usuario}
+        />
+
+        <ComponenteInput
+        estado={nombre}
+        cambiarEstado={cambiarNombre}
+        tipo="text"
+         label="Nombre"
+         placeholder="Jhon_conor"
+         name="usuario"
+         leyendaError="El nombre puede contener letras y espacios"
+         expresionRegular={expresiones.nombre}
+        />
+
+      <ComponenteInput
+        estado={password}
+        cambiarEstado={cambiarPassword}
+        tipo="password"
+         label="Contraseña"
+         
+         name="password1"
+         leyendaError="La contraseña tiene que ser de 4 a 12 digitos"
+         expresionRegular={expresiones.password}
+        />
+
+      <ComponenteInput
+        estado={password2}
+        cambiarEstado={cambiarPassword2}
+        tipo="password"
+         label="Repetir Contraseña"
+         
+         name="password2"
+         leyendaError="Ambas contraseñas deben ser iguales."
+        funcion={validarPassword2}
+         //expresionRegular={expresiones.nombre}
+        />
+
+<ComponenteInput
+        estado={correo}
+        cambiarEstado={cambiarCorreo}
+        tipo="email"
+         label="Correo Electronico"
+         placeholder="Jhon_conor@gmail.com"
+         name="correo"
+         leyendaError="El correo solo puede contener letras, numeros y puntos"
+         expresionRegular={expresiones.correo}
+        />
+        
+        <ComponenteInput
+        estado={telefono}
+        cambiarEstado={cambiarTelefono}
+        tipo="number"
+         label="Telefono"
+         placeholder="0000000"
+         name="telefono"
+         leyendaError="Solo puede contener numeros"
+         expresionRegular={expresiones.telefono}
+        />
+       
+        
+
+        <ContenedorTerminos>
+          <Label>
+            <input 
+            type="checkbox"
+             name="terminos"
+              id="terminos"
+               checked={terminos}
+               onChange={onchangeTerminos}
+               />
+            Acepto los términos y condiciones
+          </Label>
+        </ContenedorTerminos>
+
+       {formularioValido === false && <MensajeError>
+          <p>
+            <FontAwesomeIcon icon={faExclamationTriangle} />
+            <b className='errorLabel'>Error: </b>Por favor rellene el formulario correctamente
+          </p>
+        </MensajeError>}
+
+        <ContenedorBotonCentrado>
+          <Boton type='submit'>Enviar</Boton>
+         
+          {mostrarMensajeExito && <MensajeExito><span>El formulario se envio correctamente..!</span></MensajeExito>}
+
+        </ContenedorBotonCentrado>
+      </Formulario>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ src/elementos/Formularios.js
@@ -0,0 +1,169 @@
+
+import styled, {css} from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const colores={
+  borde: "#0075ff",
+  error: "#bb2929",
+  exito: "#1ed12d"
+}
+
+//los elementos se va trabajar con style component
+const Formulario = styled.form`
+  display:grid;
+  background: #ccc;
+  grid-template-columns: 1fr 1fr;
+  gap: 20px;
+
+  @media (max-width: 800px){
+    grid-template-columns:1fr;
+  }
+`;
+
+const Label=styled.label`
+  display: block;
+  font-weight: 700;
+  padding:10px;
+  min-height: 40px;
+  cursor: pointer;
+  ${props=>props.valido==='false' && css`
+    color:${colores.error};
+  `}
+`;
+const GrupoInput=styled.div`
+  position: relative;
+  z-index:90;
+`;
+
+const Input=styled.input`
+width:100%;
+background: #fff;
+border-radius: 3px;
+height: 45px;
+line-height: 45px;
+padding: 0 40px o 10px;
+transition: .3s ease all;
+border: 3px solid transparent;
+
+&:focus{
+  border: 3px solid ${colores.borde};
+  outline: none;
+  box-shadow: 3px 0px 30px rgba(163, 163, 163, 0.4);
+}
+${props => props.valido === 'true' && css`
+border: 3px solid transparent;
+`}
+${props => props.valido === 'false' && css`
+border: 3px solid ${colores.error} !important;
+`}
+`;
+
+const LeyendaError = styled.p`
+  font-size: 12px;
+  margin-bottom: 0;
+  color: ${colores.error};
+  display: none;
+
+  ${props => props.valido === 'true' && css`
+    display: none;
+`}
+
+${props => props.valido === 'false' && css`
+  display: block;
+`}
+`;
+const IconoValidacion = styled(FontAwesomeIcon)`
+  position: absolute;
+  right: 10px;
+  bottom: 14px;
+  z-index: 100;
+  font-size: 16px;
+  opacity: 0;
+
+  ${props => props.valido === 'false' && css`
+    opacity: 1;
+    color: ${colores.error};
+`}
+
+${props => props.valido === 'true' && css`
+opacity: 1;
+color: ${colores.exito};
+`}
+
+
+
+`;
+
+const ContenedorTerminos = styled.div`
+  grid-column: span 2;
+  input {
+    margin-right: 10px;
+  }
+
+  @media (max-width: 800px){
+    grid-column: span 1;
+  }
+`;
+
+const ContenedorBotonCentrado = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+grid-column: span 2;
+
+@media (max-width: 800px){
+  grid-column: span 1;
+}
+`;
+
+const Boton= styled.button`
+height: 45px;
+line-height: 45px;
+width: 30%;
+background: #000;
+color: #fff;
+font-weight: bold;
+border: none;
+border-radius: 3px;
+cursor: pinter;
+transition: .1s ease all;
+
+&:hover{
+  box-shadow: 3px 0px 30px rgba(163, 163, 163, 1);
+}
+`;
+const MensajeExito = styled.p`
+height:35px;
+line-height:35px;
+font-size: 14px;
+background: ${colores.exito};
+padding: 0px 15px;
+border-radius:3px;
+grid-column: span 2;
+span{
+  color: #fff;
+}
+*/display:none;*/
+`;
+
+const MensajeError=styled.div`
+height:45px;
+line-height:45px;
+background-color:#f66060;
+padding: 0px 15px;
+border-radius:3px;
+grid-column: span 2;
+p{
+  margin: 0;
+  
+}
+
+.errorLabel {
+  margin-left: 10px;
+}
+
+`;
+
+export {Formulario, Label, GrupoInput, Input, LeyendaError, 
+  ContenedorTerminos, ContenedorBotonCentrado, Boton, IconoValidacion,
+  MensajeExito,MensajeError};
